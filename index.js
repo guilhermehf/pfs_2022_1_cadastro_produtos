cff8d56594441405420e70c90c6a19359b20e1bb
Guilherme <guilhermehf23@gmail.com>
2022-03-28 00:37:38 -0300

    Criacao das Rotas de APIs de Cadastro

--- index.js
+++ index.js
@@ -3,11 +3,47 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+let listaProdutos = [
+  {id:1, nome:"Pastel", preco:7}
+];
+
+let idAutoIncrement = 2;
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get('/api/produtos', (req, res) => {
+  res.json(listaProdutos);
+})
+
+app.get('/api/produtos/:id', (req, res) => {
+  const id = req.params.id;
+
+  const produto = listaProdutos.find(
+    (produto) => produto.id == id
+      );
+
+  if(!produto){
+    res.status(404).json({erro:"Produto não encontrado"});
+  }
+  else{
+    res.json(produto);
+  }
+})
+
+app.post("/api/produtos", (req, res) => {
+  const produto = req.body;
+  if(produto && produto.nome && produto.preco){
+    produto.id = idAutoIncrement++;
+    listaProdutos.push(produto);  
+    res.status(201).json(produto);
+  }
+  else{
+    res.status(400).json({erro:"Falta parametro de produto"})
+  }
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
 
